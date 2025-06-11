f8143e8c41118c448524ef47bb22ae21d3ad3eb5
Sergio Radigales <sergioradigales@gmail.com>
2025-06-11 21:51:32 +0200

    home screen layout beta ready

--- app/index.tsx
+++ app/index.tsx
@@ -1,29 +1,34 @@
 import { Link } from "expo-router";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   return (
-    <View style={styles.main}>
-      <View>
-        <View style={styles.messageContainer}>
+    <SafeAreaView style={styles.main}>
+      <View style={styles.messageContainer}>
+        <View>
           <Text style={styles.individualMessage}>Plan.</Text>
         </View>
-        <View style={styles.messageContainer}>
+        <View>
           <Text style={styles.individualMessage}>Book.</Text>
         </View>
-        <View style={styles.messageContainer}>
+        <View>
           <Text style={styles.individualMessage}>Manage.</Text>
         </View>
       </View>
       <View style={styles.buttonsContainer}>
         <Link href="/sign-up" asChild>
-          <Button title="Sign up" />
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Sign up</Text>
+          </Pressable>
         </Link>
         <Link href="/log-in" asChild>
-          <Button title="Log in" />
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Log in</Text>
+          </Pressable>
         </Link>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
 
@@ -36,16 +41,30 @@ const styles = StyleSheet.create({
   messageContainer: {
     paddingTop: 50,
     width: "100%",
+    display: "flex",
+    flexDirection: "column",
   },
   individualMessage: {
     textAlign: "center",
     fontWeight: "bold",
     fontSize: 24,
+    paddingBottom: 15,
   },
   buttonsContainer: {
     display: "flex",
     flexDirection: "column",
     gap: 10,
     paddingBottom: 150,
+  },
+  button: {
+    backgroundColor: "#007AFF",
+    padding: 12,
+    borderRadius: 8,
+    alignItems: "center",
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
